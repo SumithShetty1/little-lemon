1e55ba30f211df29f02d5426cf64bc1d3681961c
SumithShetty1 <sumithshetty014@gmail.com>
2023-10-09 23:20:56 +0530

    Completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import BookingForm from './components/BookingForm';
+import BookingForm from './assets/components/BookingForm';
 
 test('Renders the BookingForm heading', () => {
-    render(<BookingForm />);
+    // Mock the required props
+    const props = {
+        availableTimes: [], // Provide an empty array or mock data as needed
+    };
+
+    render(<BookingForm {...props} />);
     const headingElement = screen.getByText("Reservations");
     expect(headingElement).toBeInTheDocument();
-})
-
+});
